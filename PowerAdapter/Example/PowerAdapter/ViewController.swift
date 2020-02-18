b1afd9794761f8525f9bd693cfbde4dede6b8589
Prashant Rathore <prashant.rathore>
2020-02-18 11:36:50 +0530

    Converted to pods

---
+++ PowerAdapter/Example/PowerAdapter/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PowerAdapter
+//
+//  Created by masterprash2 on 02/18/2020.
+//  Copyright (c) 2020 masterprash2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
