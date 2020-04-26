ef547bd837826125d1cd12a99a7db19bf95e312b
Prashant Rathore <prashant.rathore>
2020-04-26 22:13:53 +0530

    Added binding AppDelegate as parent

--- Example/PowerAdapter/AppDelegate.swift
+++ Example/PowerAdapter/AppDelegate.swift
@@ -7,17 +7,22 @@
 //
 
 import UIKit
+import PowerAdapter
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, PAParent {
 
     var window: UIWindow?
 
+    private let lifecycleRegistery = PALifecycleRegistry()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        lifecycleRegistery.viewWillAppear()
         return true
     }
+    
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -27,10 +32,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        lifecycleRegistery.viewDidDisappear()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        lifecycleRegistery.viewWillAppear()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -39,6 +46,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func getLifecycle() -> PALifecycle {
+        return lifecycleRegistery.lifecycle
+    }
+    
+    func getRootParent() -> PAParent {
+        self
+    }
+    
+    func getParent() -> PAParent? {
+        nil
     }
 
 
--- PowerAdapter/Classes/iOS/PAViewController.swift
+++ PowerAdapter/Classes/iOS/PAViewController.swift
@@ -7,24 +7,63 @@
 
 import Foundation
 import UIKit
+import RxSwift
 
 open class PAViewController : UIViewController, PAParent {
     
     private let lifecycleRegistery = PALifecycleRegistry()
+    private var isInView = false
+    private let parentLifecycle : PALifecycle? = (UIApplication.shared.delegate as? PAParent)?.getLifecycle()
+    private let disposeBag = DisposeBag()
     
     override open func viewDidLoad() {
         lifecycleRegistery.create()
         super.viewDidLoad()
+        observeParentLifecycle(parentLifecycle!)
+    }
+    
+    private func observeParentLifecycle(_ parentLifcycle : PALifecycle) {
+        parentLifecycle?.observeViewState()
+            .map({[weak self] (value) -> PALifecycle.State in
+                self?.lifecycleUpdates(value)
+                return value
+            }).subscribe().disposed(by: disposeBag)
+    }
+    
+    
+    private func lifecycleUpdates(_ state : PALifecycle.State) {
+        switch state {
+        case .resume: viewWillAppearInternal()
+        case .paused, .destroy: viewDidDisapperInternal()
+        default: return
+        }
+    }
+    
+    
+    private func viewWillAppearInternal() {
+        if(self.isInView && self.parentLifecycle?.viewState ?? .resume == PALifecycle.State.resume) {
+            self.lifecycleRegistery.viewWillAppear()
+        }
     }
     
     override open func viewWillAppear(_ animated: Bool) {
-        self.lifecycleRegistery.viewWillAppear()
+        if(!self.isInView) {
+            self.isInView = true
+            viewWillAppearInternal()
+        }
         super.viewWillAppear(animated)
     }
     
     override open func viewDidDisappear(_ animated: Bool) {
-        self.lifecycleRegistery.viewDidDisappear()
+        if(self.isInView) {
+            self.isInView = false
+            viewDidDisapperInternal()
+        }
         super.viewDidDisappear(animated)
+    }
+    
+    private func viewDidDisapperInternal() {
+        self.lifecycleRegistery.viewDidDisappear()
     }
     
     public func getLifecycle() -> PALifecycle {
@@ -39,5 +78,5 @@ open class PAViewController : UIViewController, PAParent {
         return self
     }
     
-
+    
 }
